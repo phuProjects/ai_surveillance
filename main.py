8f6feb64e8b5232465cef8c7d5df50f69be46a3c
Phu Le <phu3262006@gmail.com>
2025-07-19 14:20:56 -0400

    test

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-test
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Cannot receive frame")
+        break
+
+    cv2.imshow('Webcam Feed', frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
