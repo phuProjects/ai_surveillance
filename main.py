a6bd5745cafbb077a14961157d20f2f2a9bfc1a5
Phu Le <phu3262006@gmail.com>
2025-07-21 00:17:18 -0400

    Feature: Sound alert whenever bottle is present in the webcam

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 
 model = YOLO("yolov8n.pt")
 
-ALERT_SOUND = os.path.join("alrts", "bloop_x.wav")
-TARGET_OBJECTS = {"person"}
+ALERT_SOUND = os.path.join("alrts", "bloop_x.wav") #Using os make program compatible with other operating systems
+TARGET_OBJECTS = {"bottle"} #Add more objects later on
 
 alert_triggered = False
 
@@ -23,6 +23,23 @@ while True:
 
     results = model(frame)
     annotated_frame = results[0].plot()
+
+    #Checking for detections
+    for result in results:
+        for box in result.boxes:
+            cls_id = int(box.cls[0])
+            label = model.names[cls_id]
+
+            if label in TARGET_OBJECTS:
+                print(f"ALERT {label} detected!!!")
+
+                if not alert_triggered:
+                    playsound(ALERT_SOUND)
+                    alert_triggered = True
+                    
+    alert_triggered = False
+
+
     cv2.imshow("YOLOv8 Surveillance", annotated_frame)
 
     if cv2.waitKey(1) == 27:  # Press 'q' to quit
